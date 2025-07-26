607d8fbf9892934691f239e33088007180fc57ec
datta4721 <dattatryasitsolutions@gmail.com>
2025-07-26 18:09:18 +0530

    base url change

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,8 +16,8 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://192.168.1.21/canatech/';
-    // public string $baseURL = 'http://canatech.satarait.com/';
+    // public string $baseURL = 'http://192.168.1.21/canatech/';
+    public string $baseURL = 'http://canatech.satarait.com/';
 
 
     /**
